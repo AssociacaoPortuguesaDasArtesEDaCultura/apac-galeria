8d7916827358464c539f0f2fd3196e7651eeb46d
Bernardo Silva <bernardofasilva@protonmail.com>
2024-02-27 18:26:54 +0000

    Fixed loading user data

--- client/src/components/pintar_o_7/ArtistThumbnail.tsx
+++ client/src/components/pintar_o_7/ArtistThumbnail.tsx
@@ -6,44 +6,44 @@ import { checkLink } from '../../fetchers';
 import { Seller } from '../../types/user';
 
 type ArtistThumbnailProps = {
-  artist: Seller
-}
+    artist: Seller;
+};
 
 export default function ArtistThumbnail(props: ArtistThumbnailProps) {
-  const seller: Seller = props.artist;
-  const name = seller.name;
-  const length = name.split(' ').length;
+    const seller: Seller = props.artist;
+    const name = seller.name;
+    const length = name.split(' ').length;
 
-  const lastName = name.split(' ')[length - 1];
-  const otherNames = name
-    .split(' ')
-    .slice(0, length - 1)
-    .join(' ');
-  return (
-    <Box component="div">
-      <Typography variant="subtitle1">{otherNames}</Typography>
-      <Typography variant="h2" fontWeight="bold">
-        {lastName}
-      </Typography>
-      <Link to={`/artists/${seller.id}`} state={seller}>
-        <Box component="div" className="w-full aspect-square">
-          <img
-            className="w-full h-full aspect-square object-cover"
-            src={checkLink(seller.profilePicture)}
-          />
+    const lastName = name.split(' ')[length - 1];
+    const otherNames = name
+        .split(' ')
+        .slice(0, length - 1)
+        .join(' ');
+    return (
+        <Box component="div">
+            <Typography variant="subtitle1">{otherNames}</Typography>
+            <Typography variant="h2" fontWeight="bold">
+                {lastName}
+            </Typography>
+            <Link to={`/artists/${seller.id}`} state={seller}>
+                <Box component="div" className="w-full aspect-square">
+                    <img
+                        className="w-full h-full aspect-square object-cover"
+                        src={checkLink(seller.profilePicture)}
+                    />
+                </Box>
+            </Link>
+            <Box component="div">
+                <Grid
+                    container
+                    spacing={2}
+                    sx={{
+                        justifyContent: 'space-between',
+                        marginTop: 0,
+                    }}>
+                    <Grid xs={12}>{seller.about}</Grid>
+                </Grid>
+            </Box>
         </Box>
-      </Link>
-      <Box component="div">
-        <Grid
-          container
-          spacing={2}
-          sx={{
-            justifyContent: 'space-between',
-            marginTop: 0,
-          }}>
-          <Grid xs={12}>{seller.about}</Grid>
-        </Grid>
-      </Box>
-    </Box>
-  );
+    );
 }
--- client/src/components/pintar_o_7/Hero.tsx
+++ client/src/components/pintar_o_7/Hero.tsx
@@ -1,60 +1,60 @@
 import { Box, Typography } from '@mui/material';
 
 type HeroProps = {
-  title: string,
-  subtitle: string,
-  img: string,
-  color: string,
-  fontColor: string
-}
+    title: string;
+    subtitle: string;
+    img: string;
+    color: string;
+    fontColor: string;
+};
 
 export default function Hero(props: HeroProps) {
-  return (
-    <Box
-      component="div"
-      sx={{
-        display: 'flex',
-        flexDirection: { md: 'row', xs: 'column' },
-        justifyContent: 'center',
-        width: '100%',
-      }}>
-      <Box component="div" sx={{ display: 'flex' }}>
-        <img
-          src={props.img}
-          style={{
-            maxHeight: '35rem',
-            width: '100%',
-            height: 'auto',
-            objectFit: 'cover',
-            backgroundColor: props.color,
-          }}
-        />
-      </Box>
-      <Box
-        component="div"
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          p: { md: 10, xs: 5 },
-          flexGrow: 1,
-          color: props.fontColor || '#FFF6DE',
-          backgroundColor: props.color,
-          alignItems: { xs: 'center', md: 'start' },
-          justifyContent: { xs: 'center', md: 'end' },
-        }}>
-        <Typography
-          variant="h2"
-          fontWeight="bold"
-          textAlign={{ md: 'left', xs: 'center' }}
-          className="text-3xl md:text-5xl">
-          {props.title}
-        </Typography>
-        <Typography
-          textAlign={{ md: 'left', sm: 'center' }}
-          marginTop={2}>
-          {props.subtitle}
-        </Typography>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box
+            component="div"
+            sx={{
+                display: 'flex',
+                flexDirection: { md: 'row', xs: 'column' },
+                justifyContent: 'center',
+                width: '100%',
+            }}>
+            <Box component="div" sx={{ display: 'flex' }}>
+                <img
+                    src={props.img}
+                    style={{
+                        maxHeight: '35rem',
+                        width: '100%',
+                        height: 'auto',
+                        objectFit: 'cover',
+                        backgroundColor: props.color,
+                    }}
+                />
+            </Box>
+            <Box
+                component="div"
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    p: { md: 10, xs: 5 },
+                    flexGrow: 1,
+                    color: props.fontColor || '#FFF6DE',
+                    backgroundColor: props.color,
+                    alignItems: { xs: 'center', md: 'start' },
+                    justifyContent: { xs: 'center', md: 'end' },
+                }}>
+                <Typography
+                    variant="h2"
+                    fontWeight="bold"
+                    textAlign={{ md: 'left', xs: 'center' }}
+                    className="text-3xl md:text-5xl">
+                    {props.title}
+                </Typography>
+                <Typography
+                    textAlign={{ md: 'left', sm: 'center' }}
+                    marginTop={2}>
+                    {props.subtitle}
+                </Typography>
+            </Box>
+        </Box>
+    );
 }
