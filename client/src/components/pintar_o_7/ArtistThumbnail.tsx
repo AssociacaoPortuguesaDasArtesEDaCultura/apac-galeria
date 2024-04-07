9933a3b1d79642f9b199a1569324874dc53a0806
Bernardo Silva <bernardofasilva@protonmail.com>
2024-04-07 16:44:35 +0100

    Simplified artist thumbnail

--- client/src/components/pintar_o_7/ArtistThumbnail.tsx
+++ client/src/components/pintar_o_7/ArtistThumbnail.tsx
@@ -1,8 +1,6 @@
 import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
 import { Typography } from '@mui/material';
-import { checkLink } from '../../fetchers';
 import { Seller } from '../../types/user';
 
 type ArtistThumbnailProps = {
@@ -11,39 +9,25 @@ type ArtistThumbnailProps = {
 
 export default function ArtistThumbnail(props: ArtistThumbnailProps) {
     const seller: Seller = props.artist;
-    const name = seller.name;
-    const length = name.split(' ').length;
+    const names = seller.name.split(' ');
 
-    const lastName = name.split(' ')[length - 1];
-    const otherNames = name
-        .split(' ')
-        .slice(0, length - 1)
-        .join(' ');
+    const otherNames = names.slice(0, -1).join(' ');
+
     return (
-        <Box component="div">
+        <div>
             <Typography variant="subtitle1">{otherNames}</Typography>
             <Typography variant="h2" fontWeight="bold">
-                {lastName}
+                {names.at(-1)}
             </Typography>
             <Link to={`/artists/${seller.id}`} state={seller}>
                 <Box component="div" className="w-full aspect-square">
                     <img
                         className="w-full h-full aspect-square object-cover"
-                        src={checkLink(seller.profilePicture)}
+                        src={seller.profilePicture}
                     />
                 </Box>
             </Link>
-            <Box component="div">
-                <Grid
-                    container
-                    spacing={2}
-                    sx={{
-                        justifyContent: 'space-between',
-                        marginTop: 0,
-                    }}>
-                    <Grid xs={12}>{seller.about}</Grid>
-                </Grid>
-            </Box>
-        </Box>
+            <Typography sx={{ marginTop: 0 }}> {seller.about} </Typography>
+        </div>
     );
 }
